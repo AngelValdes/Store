251bff23c671587d77d51ff8cd4947cd38f33121
Angel Valdes <AngelValdes@hotmail.com>
2016-08-03 00:49:25 -0400

    Add project files.

---
+++ Store/index.js
@@ -0,0 +1,10 @@
+﻿(function (controllers) {
+    "use strict";
+    var usersController = require("./api/usersController");
+    var productsController = require("./api/productsController");
+
+    controllers.init = function (app) {
+        usersController.init(app);
+        productsController.init(app);
+    };
+})(module.exports);
