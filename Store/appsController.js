7aef3f86431a1e90333d6a7b8badba0c62a991e9
AngelValdes <AngelValdes@DESKTOP-4FGUIM4>
2016-08-12 11:51:28 -0400

    controllers updates

--- Store/appsController.js
+++ Store/appsController.js
@@ -1,6 +1,8 @@
 ﻿(function (appsController) {
     "use strict";
     appsController.init = function (app) {
+        //load and initialize logger
+        var logger = require("./logger.js");
         //configuration of json formatter
         var bodyParser = require("body-parser");
         app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,49 +16,76 @@
         //Read all
         app.get("/api/v1/applications", function (request, response) { //route
             response.set("Content-Type", "application/json"); //set data format to return
-            model.Applications().findAll({ include: [model.Users(), model.ArtAssets()] }).then(function (applications) {
-                response.send(applications); //send data
-            });
+            model.Applications().findAll({ include: [model.Users(), model.ArtAssets()] })
+                .then(function (applications) {
+                    logger.debug("All applications read :" + JSON.stringify(applications) + "\n", 0);
+                    response.status(200).send([200, applications, null]); //send data   
+                })
+                .catch(function (error) {
+                    logger.debug("All applications read error: " + error + "\n", 2);
+                    response.status(500).send([500, null, null]);
+                });
         });
 
         //Read by id
         app.get("/api/v1/applications/:id", function (request, response) { //route
             var id = request.params.id; //get id value from route params
             response.set("Content-Type", "application/json"); //set data format to return
-            model.Applications().findAll({ where: { id: id }, include: [model.Users(), model.ArtAssets()] }).then(function (applications) {
-                response.send(applications); //send data
-            });
+            model.Applications().findAll({ where: { id: id }, include: [model.Users(), model.ArtAssets()] })
+                .then(function (applications) {
+                    logger.debug("by id application read :" + JSON.stringify(applications) + "\n", 0);
+                    response.status(200).send([200, applications, null]); //send data
+                })
+                .catch(function (error) {
+                    logger.debug("by id application read error: " + error + "\n", 2);
+                    response.status(500).send([500, null, null]);
+                });
 
         });
 
         //Create
         app.post("/api/v1/applications", function (request, response) { //route
-            var title = request.body.title; //get values from body in request
-            var description = request.body.description;
-            var releaseDate = request.body.releaseDate;
-            var userId = request.body.userId;
-            var application = { "title": title, "description": description, "releaseDate": releaseDate, "userId": userId } //compose new item
-            model.Applications().create(application); //add item to list
-            response.send([201, application, null]); //return created
+            //get values from body in request
+            var application = { "title": request.body.title, "description": request.body.description, "releaseDate": request.body.releaseDate, "userId": request.body.userId } //compose new item
+            model.Applications().create(application)
+                .then(function () {//add item to list 
+                    logger.debug("Application created :" + JSON.stringify(application) + "\n", 0);
+                    response.status(201).send([201, application, null]); //return created
+                })
+                .catch(function (error) {
+                    logger.debug("Creating application error: " + error + "\n", 2);
+                    response.status(500).send([500, null, null]);
+                });           
         });
 
         //update
         app.put("/api/v1/applications/:id", function (request, response) { //route
             var id = request.params.id; //get id value from route params
-            var title = request.body.title; //get values from body in request
-            var description = request.body.description;
-            var releaseDate = request.body.releaseDate;
-            var userId = request.body.userId;
-            var application = { "title": title, "description": description, "releaseDate": releaseDate, "userId": userId } //compose new item
-            model.Applications().update(application, { where: { id: id } }); //update item
-            response.send([301, application, null]); //return modified
+            var application = { "title": request.body.title, "description": request.body.description, "releaseDate": request.body.releaseDate, "userId": request.body.userId } //compose new item
+            model.Applications().update(application, { where: { id: id } })
+                .then(function () {//update item
+                    logger.debug("Application updated: " + JSON.stringify(application) + "\n", 0);
+                    response.status(301).send([301, application, null]); //return modified
+                })
+                .catch(function (error) {
+                    logger.debug("Updating user error: " + error + "\n", 2);
+                    response.status(500).send([500, null, null]);
+                }); 
+
         });
 
         //Delete
         app.delete("/api/v1/applications/:id", function (request, response) { //route
             var id = request.params.id; //get id value from route params
-            model.Applications().destroy({ where: { id: id } }); //delete item
-            response.send([301, null, null]); //return deleted         
+            model.Applications().destroy({ where: { id: id } })
+                .then(function () {//delete item
+                    logger.debug("Application deleted: id = " + id + "\n", 0);
+                    response.status(301).send([301, id, null]); //return deleted
+                })
+                .catch(function (error) {
+                    logger.debug("Deleting application error: " + error + "\n", 2);
+                    response.status(500).send([500, null, null]);
+                });         
         });
 
     }
--- Store/usersController.js
+++ Store/usersController.js
@@ -2,7 +2,7 @@
     "use strict";
     usersController.init = function (app) {
         //load and initialize logger
-        var Logger = require("./logger.js");
+        var logger = require("./logger.js");
         //configuration of json formatter
         var bodyParser = require("body-parser");
         app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,10 +17,10 @@
         app.get("/api/v1/users", function (request, response) { //route
             response.set("Content-Type", "application/json"); //set data format to return
             model.Users().findAll({ include: [model.Applications()], raw: true }).then(function (users) {
-                Logger.debug("All users read :" + JSON.stringify(users) + "\n", 0);
+                logger.debug("All users read :" + JSON.stringify(users) + "\n", 0);
                 response.status(200).send([200,users, null]); //send data             
             }).catch(function(error) {
-                Logger.debug("All users read error: " + error + "\n", 2);
+                logger.debug("All users read error: " + error + "\n", 2);
                 response.status(500).send([500, null, null]);
             });          
         });
@@ -29,13 +29,12 @@
         app.get("/api/v1/users/:id", function (request, response) { //route
             var id = request.params.id; //get id value from route params
             response.set("Content-Type", "application/json"); //set data format to return
-            model.Users()
-                .findAll({ where: { id: id }, include: [model.Applications()] })
+            model.Users().findAll({ where: { id: id }, include: [model.Applications()], raw: true  })
                 .then(function (users) {
-                    Logger.debug("by id user read :" + JSON.stringify(users) + "\n", 0);
+                    logger.debug("by id user read :" + JSON.stringify(users) + "\n", 0);
                     response.status(200).send([200, users, null]); //send data
                 }).catch(function (error) {
-                    Logger.debug("by id user read error: " + error + "\n", 2);
+                    logger.debug("by id user read error: " + error + "\n", 2);
                     response.status(500).send([500, null, null]);
                 });             
         });
@@ -44,32 +43,46 @@
         app.post("/api/v1/users", function (request, response) { //route
             var name = request.body.name; //get values from body in request
             var user = { "name": name } //compose new item
-            model.Users().create(user).then(
-                function (data) {
-                    Logger.debug("user created :" + JSON.stringify(user) + "\n", 0);
+            model.Users().create(user)
+                .then(function () {//add item to list 
+                    logger.debug("user created :" + JSON.stringify(user) + "\n", 0);
                     response.status(201).send([201, user, null]); //return created
                 })
                 .catch(function (error) {
-                    Logger.debug("Creating user error: " + error + "\n", 2);
+                    logger.debug("Creating user error: " + error + "\n", 2);
                     response.status(500).send([500, null, null]);
-                }); //add item to list            
+                });            
         });
  
         //update
         app.put("/api/v1/users/:id", function (request, response) { //route
             var id = request.params.id; //get id value from route params
-            var name = request.body.name; //get values from body in request
-            var user = { "name": name } //compose item
-            model.Users().update(user, { where: { id: id } }); //update item
-            Logger.debug("user updated \n" + JSON.stringify(user), 0);
-            response.send([301, user, null]); //return modified
+            var user = { "name": request.body.name } //compose item
+            model.Users().update(user, { where: { id: id } })
+                .then(function () {//update item
+                    logger.debug("user updated: " + JSON.stringify(user) + "\n", 0);
+                    response.status(301).send([301, user, null]); //return modified
+                })
+                .catch(function (error) {
+                    logger.debug("Updating user error: " + error + "\n", 2);
+                    response.status(500).send([500, null, null]);
+                }); 
+           
         });
 
         //Delete
         app.delete("/api/v1/users/:id", function (request, response) { //route
             var id = request.params.id; //get id value from route params
-            model.Users().destroy({ where: { id: id } }); //delete item
-            response.send([301, null, null]); //return deleted         
+            model.Users().destroy({ where: { id: id } })
+                .then(function () {//delete item
+                    logger.debug("user deleted: id = " + id + "\n", 0);
+                    response.status(301).send([301, id, null]); //return deleted
+                })
+                .catch(function (error) {
+                    logger.debug("Deleting user error: " + error + "\n", 2);
+                    response.status(500).send([500, null, null]);
+                }); 
+                    
         });
         
     }
