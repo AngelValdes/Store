ba72f23f3139074ca43bef643ddede0ad0ec7ac6
AngelValdes <AngelValdes@DESKTOP-4FGUIM4>
2016-08-16 08:59:13 -0400

    some minor changes

--- Store/server.js
+++ Store/server.js
@@ -7,11 +7,10 @@ const port = process.env.PORT || 3000;
 const app = express();  // instantiate application
 
 app.use(bodyParser.json()); // to get json from req body
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', require('./routes')(express)); // initialize routes
 
 const logger = require('./services/logger');
 
-module.exports = app.listen(port,
-() => logger.debug('Sever Active on:' + port, 0)); // initialize server, exception to rule.
+module.exports = app.listen(port, () => logger.debug('Sever Active on:' + port, 0)); // initialize server, exception to rule.
 
