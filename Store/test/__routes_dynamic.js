8faf535a65ceef2a27622228a26b767de26c532a
AngelValdes <AngelValdes@DESKTOP-4FGUIM4>
2016-08-18 19:03:00 -0400

    dynamic testing of routes

--- Store/test/__routes_dynamic.js
+++ Store/test/__routes_dynamic.js
@@ -5,33 +5,96 @@ const User = require('../models/user');
 const faker = require('faker');
 
 describe('dynamic Routes', () => {
-  /* eslint no-var: 0 */
-  var server;
-  /* eslint no-undef: 0 */
-  beforeEach(() => { // reconfigure server before each test to isolate them
-    server = require('../server');
-  });
-  afterEach(() => {
-    server.close();
-  });
-    var testRoutes = [
-        { resource: "users", name: 'userGetAll', httpVerb: 'GET', route: '/api/v1/users' },
-        { resource: "users", name: 'userGetById', httpVerb: 'GET', route: '/api/v1/users/:id' },
-        { resource: "apps", name: 'appGetAll', httpVerb: 'GET', route: '/api/v1/apps' },
-        { resource: "apps", name: 'appGetById', httpVerb: 'GET', route: '/api/v1/apps/:id' },
+    /* eslint no-var: 0 */
+    var server;
+    var tempData;
+    /* eslint no-undef: 0 */
+    beforeEach(() => { // reconfigure server before each test to isolate them
+        server = require('../server');
+    });
+    afterEach(() => {
+        server.close();
+    });
+    const tests = [
+        {
+            resource: "users",
+            name: 'userGetAll',
+            httpVerb: 'GET',
+            route: '/api/v1/users',
+            description: "Gets the list of all users",
+            conditions: (res) => {
+                chai.expect(res.body.length).to.be.above(0);
+            }
+        },
+        {
+            resource: "users",
+            name: 'createUser',
+            httpVerb: 'POST',
+            route: '/api/v1/users',
+            description: "creates new user",
+            conditions: (res, fake) => {
+                tempData = res.body.id;
+                chai.expect(res.body.name).to.be.equal(fake.name);
+            },
+            fake: { name: faker.name.firstName() }
+        },      
+        {
+            resource: "apps",
+            name: 'createapp',
+            httpVerb: 'POST',
+            route: '/api/v1/apps',
+            description: "creates new app",
+            conditions: (res, fake) => {
+                tempData = res.body;
+                chai.expect(res.body.title).to.be.equal(fake.title);
+                chai.expect(res.body).to.have.property('id');
+                chai.expect(res.body).to.have.property('title');
+                chai.expect(res.body).to.have.property('description');
+            },
+            fake: { title: faker.name.firstName(), description: "Some new app" }
+        },
+        {
+            resource: "apps",
+            name: 'appsGetAll',
+            httpVerb: 'GET',
+            route: '/api/v1/apps',
+            description: "Gets the list of all apps",
+            conditions: (res) => {
+                chai.expect(res.body.length).to.be.above(0);
+            }
+        }
     ];
-    // Test dynamic routes ... under construction!
-  //it('GET routes', (done) => {
-  //    request(server)
-  //        .get(() => {
-  //            for (var i = 0; i < testRoutes.length; i++) {
-                  
-  //            }
-  //        })
-  //      .set('Accept', 'application/json')        
-  //      .expect((res) => {
-  //      })
-  //      .end(done);
-  //});
-    
+    tests.forEach((test) => {
+        it(test.name + ' - ' + test.description, (done) => {
+
+            switch (test.httpVerb) {
+                case 'GET':
+                    request(server)
+                        .get(test.route)
+                        .set('Accept', 'application/json')
+                        .expect('Content-Type', /json/)
+                        .expect((res) => {
+                            test.conditions(res);
+                        })
+                        .end(done);
+                    break;
+                case 'POST':
+                    request(server)
+                        .post(test.route)
+                        .set('Accept', 'application/json')
+                        .send(test.fake)
+                        .expect('Content-Type', /json/)
+                        .expect((res) => {
+                            test.conditions(res, test.fake);
+                        })
+                        .end(done);
+                    break;
+                case 'PUT':
+                    break;
+                case 'DELETE':
+                    break;
+            }
+        });
+    });
+
 });
