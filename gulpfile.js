1408d92627d04753f22bc40f71b5148b1321af18
Angel Valdes <Angel Valdes>
2016-08-29 23:27:23 -0400

    packages

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const utility = require('simple-logger-pkg');
+const packageJson = require('./package.json');
+const fs = require('fs');
+
+gulp.task('default', [], () => {
+  console.log('running default task');
+});
+gulp.task('versionUp', [], () => {
+  console.log('running versionUp task');
+  const newVersion = utility.versionUp(packageJson.version, process.argv[4]); // "Major" | "Minor" | "Patch"
+  console.log('New version is ' + newVersion);
+  packageJson.version = newVersion;
+  fs.writeFile('package.json', JSON.stringify(packageJson), (err) => {
+    if (err) return console.log(err);
+    // console.log(JSON.stringify(file));
+    return console.log('writing to package.json, version: ' + newVersion);
+  });
+  // console.log(process.argv[4]);
+});
